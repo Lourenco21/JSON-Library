d7215fd92fb0e8a52953dab758a48b86f75ae3f8
Lourenco21 <lcfvs@iscte-iul.pt>
2025-05-14 13:21:58 +0100

    GetJson Framework started

---
+++ src/GetJson.kt
@@ -0,0 +1,29 @@
+annotation class Mapping(val endpoint: String)
+
+annotation class Path()
+
+annotation class Param()
+
+@Mapping("api")
+class Controller {
+    @Mapping("ints")
+    fun demo(): List<Int> = listOf(1, 2, 3)
+
+    @Mapping("pair")
+    fun obj(): Pair<String, String> = Pair("um", "dois")
+
+    @Mapping("path/{pathvar}")
+    fun path(
+        @Path pathvar: String
+    ): String = "$pathvar!"
+
+    @Mapping("args")
+    fun args(
+        @Param n: Int,
+        @Param text: String
+    ): Map<String, String> = mapOf(text to text.repeat(n))
+}
+
+
+
+
