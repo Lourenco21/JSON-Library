907766e8778c5d9623e8d0b6134dca58def323b2
Lourenco21 <lcfvs@iscte-iul.pt>
2025-05-14 19:23:22 +0100

    Args for GetJson done

--- src/GetJson.kt
+++ src/GetJson.kt
@@ -1,8 +1,12 @@
+import kotlin.collections.set
 import kotlin.reflect.KClass
+import kotlin.reflect.KParameter
+import kotlin.reflect.KType
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.functions
 import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.instanceParameter
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class Mapping(val endpoint: String)
@@ -14,40 +18,80 @@ annotation class Path()
 annotation class Param()
 
 
-class GetJson(vararg val controllers: KClass<*>){
+class GetJson(vararg val controllers: KClass<*>) {
 
-    fun getPaths() : Map<String, Pair<String, Any>>{
-        val paths = mutableMapOf<String, Pair<String,Any>>()
+    fun getPaths(): Map<String, Pair<String, Any>> {
+        val paths = mutableMapOf<String, Pair<String, Any>>()
 
-
-        controllers.forEach{ c ->
+        controllers.forEach { c ->
             val controllerInstance = c.createInstance()
             var path = "/" + c.findAnnotation<Mapping>()?.endpoint.toString() + "/"
             c.functions.forEach {
-                if(it.hasAnnotation<Mapping>()){
-                    paths.put(path + it.findAnnotation<Mapping>()?.endpoint.toString(),  Pair(it.name, controllerInstance))
+                if (it.hasAnnotation<Mapping>()) {
+                    path += it.findAnnotation<Mapping>()?.endpoint.toString()
+
                 }
+
+                paths.put(path, Pair(it.name, controllerInstance))
+                path = "/" + c.findAnnotation<Mapping>()?.endpoint.toString() + "/"
             }
         }
 
         return paths
     }
 
-    fun run(path : String) : JValue{
+    fun run(path: String): JValue {
         val paths = getPaths()
-        var result : Any? = null
-
+        var result: Any? = null
+        var realPath = path
+        if (path.contains("?")) {
+            realPath = path.split("?")[0]
+        }
         paths.forEach {
             val functionToBeCalled = it.value.first
             val controllerInstance = it.value.second
-            if(it.key == path){
+            if (it.key == realPath) {
                 val function = controllerInstance::class.functions.first { f -> f.name == functionToBeCalled }
-                result = function.call(controllerInstance)
+                val realParams = function.parameters.filter { it.kind == KParameter.Kind.VALUE }
+                if (realParams.isNotEmpty()) {
+                    var args = mutableMapOf<KParameter, Any?>()
+                    args[function.instanceParameter!!] = controllerInstance
+                    function.parameters.forEach {
+                        if (it.hasAnnotation<Param>()) {
+                            args[it] = splitArgs(path, it)
+                        }
+                    }
+                    result = function.callBy(args)
+                } else {
+                    result = function.call(controllerInstance)
+                }
+
             }
         }
         return instantiateJsonModel(result)
     }
 
+    fun mapType(value: String, type: KType): Any =
+        when (type.classifier) {
+            Int::class -> value.toInt()
+            Double::class -> value.toDouble()
+            String::class -> value
+            Boolean::class -> value.toBoolean()
+            else -> TODO("unsupported: " + type.classifier.toString())
+        }
+
+    fun splitArgs(path: String, param: KParameter): Any? {
+        var arg : Any? = null
+        val pathParts = path.split('?')
+        val argsParts = pathParts[1].split("&")
+        for (part in argsParts) {
+            val keyValue = part.split("=")
+            if (param.name == keyValue[0]) {
+                 arg = mapType(keyValue[1], param.type)
+            }
+        }
+        return arg
+    }
 }
 
 @Mapping("api")
@@ -73,9 +117,9 @@ class Controller {
 
 fun main() {
     val app = GetJson(Controller::class)
-    println(app.getPaths())
-    println(app.run("/api/ints"))
-    println(app.run("/api/ints").toText())
+    println(app.getPaths().keys)
+    println(app.run("/api/args?n=3&text=PA"))
+    println(app.run("/api/args?n=3&text=PA").toText())
 }
 
 
